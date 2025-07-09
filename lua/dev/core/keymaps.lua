916420ab9622121ae2b45d90628a9eb35ca7d445
White-Devil04 <dev082004@gmail.com>
2025-07-09 23:26:46 +0530

    update: added navigation keybinding in split screen

--- lua/dev/core/keymaps.lua
+++ lua/dev/core/keymaps.lua
@@ -22,6 +22,11 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sj", "<C-w>j", { desc = "Go to split below" }) -- go to split below
+keymap.set("n", "<leader>sk", "<C-w>k", { desc = "Go to split above" }) -- go to split above
+keymap.set("n", "<leader>sl", "<C-w>l", { desc = "Go to split right" }) -- go to split right
+keymap.set("n", "<leader>sh", "<C-w>h", { desc = "Go to split left" }) -- go to split left
+keymap.set("n", "<leader>so", "<C-w>o", { desc = "Close other splits" }) -- close all other splits
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
